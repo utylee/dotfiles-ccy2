9c179a8a4a88a82c0cfb7392f9e3c6d384d188b1
utylee <utylee@gmail.com>
2026-06-11 04:12:34 +0900

    add: updated

--- .config/fish/functions/comfy.fish
+++ .config/fish/functions/comfy.fish
@@ -2,7 +2,7 @@ function comfy
     env LC_ALL=C.UTF-8 \
         LANG=C.UTF-8 \
         PYTHONIOENCODING=utf-8 \
-        python /data/temp/ComfyUI/main.py \
+        python ~/temp/ComfyUI/main.py \
             --lowvram \
             --disable-pinned-memory \
             --force-fp16 \
